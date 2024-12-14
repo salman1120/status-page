03b82f0d817e63e27ddc761e50f39ed5043ca94f
salman1120 <salluk1120@gmail.com>
2024-12-14 19:12:34 +0530

    Added Replit Launch files

--- replit.nix
+++ replit.nix
@@ -2,7 +2,7 @@
     deps = [
         pkgs.nodejs-18_x
         pkgs.nodePackages.typescript-language-server
-        pkgs.yarn
+        pkgs.nodePackages.yarn
         pkgs.replitPackages.jest
         pkgs.postgresql
     ];
